ff24864c1f1895d791f07b600cb9fb50438c0847
Bo Fjord Jensen <bo@bxd.dk>
2020-01-12 14:36:24 +0100

    Change DLL metadata, add proper license and fix the product name

--- libadikted/adikted_private.h
+++ libadikted/adikted_private.h
@@ -5,19 +5,19 @@
 #define ADIKTED_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"0.9.5.7"
+#define VER_STRING	"0.9.6.1"
 #define VER_MAJOR	0
 #define VER_MINOR	9
-#define VER_RELEASE	5
-#define VER_BUILD	7
+#define VER_RELEASE	6
+#define VER_BUILD	1
 #define COMPANY_NAME	""
 #define FILE_VERSION	""
 #define FILE_DESCRIPTION	"ADiKtEd Map Maintain Library"
 #define INTERNAL_NAME	""
-#define LEGAL_COPYRIGHT	"Open source"
+#define LEGAL_COPYRIGHT	"GNU General Public License v2.0"
 #define LEGAL_TRADEMARKS	""
 #define ORIGINAL_FILENAME	""
-#define PRODUCT_NAME	"Unofficial Bullfrog game tools"
+#define PRODUCT_NAME	"Another Dungeon Keeper Editor"
 #define PRODUCT_VERSION	""
 
 #endif /*ADIKTED_PRIVATE_H*/
