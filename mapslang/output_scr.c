5d4173e7b329541e27b7ec20d308966522b97a13
eddebaby <ed_j_kearney@yahoo.co.uk>
2020-10-03 08:59:29 +0100

    Character codes are now correctly sent to new S-Lang
    This patch:
    - Enables UTF-8 mode in libslang
    - Casts the char sent to SLsmg_write_char() as an unsigned char
    - **Doing this fixes the drawing of lines with mapslang** (Sending the charcode for any character from code page 850 works for me).
    
    An `unsigned char` matches `SLuchar_Type` from slang, the precise reason that this works is unknown to me right now. Looking through SLsmg_write_chars() in slsmg.c should lead to an answer.
    
    I'm not sure what determines the codepage here, or if it is compile time, or run time.
    
    These changes do not cause an issue if you use map.exe (with these changes) with the older libslang.dll from 0.95

--- mapslang/output_scr.c
+++ mapslang/output_scr.c
@@ -107,7 +107,7 @@ void screen_printf_toeol(char *format, ...)
 void screen_printchr(char dst)
 {
     if (!screen_initied) return;
-    SLsmg_write_char(dst);
+    SLsmg_write_char((unsigned char)dst);
 }
 
 void screen_clear(void)
